3c47037bd1b8de8836fc6cc15bddf975e18b6c90
Altrim Beqiri <altrimbeqiri@gmail.com>
2016-11-05 01:10:31 +0100

    Remove scenario 1.13 from travis

--- tests/integration/components/tool-tipster-test.js
+++ tests/integration/components/tool-tipster-test.js
@@ -6,8 +6,6 @@ moduleForComponent('tool-tipster', 'Integration | Component | tool tipster', {
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
-
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
