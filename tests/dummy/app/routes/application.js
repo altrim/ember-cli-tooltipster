c588b8c6efa8e987a8c5d98dfc5da8e12c6adfc8
Altrim Beqiri <altrimbeqiri@gmail.com>
2016-12-18 21:58:55 +0100

    update demo app ember-paper

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
-  actions: {
-    transitionTo(route) {
-      this.transitionTo(route);
-    },
-
-    linkTo(url) {
-      window.open(url);
-    }
-  }
 });
