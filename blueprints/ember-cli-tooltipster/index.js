f7f2386be131d66cbfe4e8ae6588171a1c511042
Altrim Beqiri <altrimbeqiri@gmail.com>
2017-03-16 00:09:02 +0100

    Fix addPackagesToProject in blueprint afterInstall

--- blueprints/ember-cli-tooltipster/index.js
+++ blueprints/ember-cli-tooltipster/index.js
@@ -6,7 +6,7 @@ module.exports = {
   },
 
   afterInstall: function( /*options*/ ) {
-    return this.addPackageToProject([{
+    return this.addPackagesToProject([{
       name: 'tooltipster',
       target: '4.2.2'
     }]);
